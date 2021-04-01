b7c341bdff6bdea35953030a2a94ea479ff54c7f
Jason Ours <jasonours@outlook.com>
2021-03-31 19:23:08 -0500

    building youtube api

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyADFp3RQHK3XyxkacTpM1lOgvGnbEgS0dU';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: KEY
+  }
+});
